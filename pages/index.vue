85cec71e152c370d269a69b05cea563eb95aa323
Takafumi Fukai <takyafumin@gmail.com>
2024-10-12 18:57:28 +0900

    add, HelloWorld 実装

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from '~/feature/pokemon-search/components/HelloWorld.vue'
+
+useHead({
+  title: 'ポケモン図鑑',
+})
+</script>
+
+<template>
+  <HelloWorld />
+</template>
