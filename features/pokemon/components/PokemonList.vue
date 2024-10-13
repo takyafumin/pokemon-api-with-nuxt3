4d37fad7dd9e58abb9964a8660a6450585a6758c
Takafumi Fukai <takyafumin@gmail.com>
2024-10-13 20:55:20 +0900

    modify, 一覧画面のレイアウトをVuetify3を利用するように改善

--- features/pokemon/components/PokemonList.vue
+++ features/pokemon/components/PokemonList.vue
@@ -1,27 +1,43 @@
 <script setup lang="ts">
+import { VCard, VCardTitle, VCardText, VContainer, VRow, VCol, VProgressCircular } from 'vuetify/components'
 import { useFetchPokemons } from '../composables/pokemons'
 
 const { pokemons, status, error } = await useFetchPokemons()
 </script>
 
 <template>
-  <div v-if="status === 'pending' || status === 'idle'">
-    Now Loading...
-  </div>
-  <div v-else-if="error !== null">
-    {{ error }}
-  </div>
-  <div v-else-if="pokemons.length === 0">
-    No Data
-  </div>
-  <div v-else>
-    <dl
-      v-for="pokemon in pokemons"
-      :key="pokemon.name"
-    >
-      <dd id="{{ pokemon.name }}">
-        {{ pokemon.name }} {{ pokemon.url }}
-      </dd>
-    </dl>
-  </div>
+  <VContainer>
+    <div v-if="status === 'pending' || status === 'idle'">
+      <VProgressCircular
+        indeterminate
+        color="primary"
+      >
+        Now Loading...
+      </VProgressCircular>
+    </div>
+    <div v-else-if="error !== null">
+      {{ error }}
+    </div>
+    <div v-else-if="pokemons.length === 0">
+      No Data
+    </div>
+    <div v-else>
+      <VRow>
+        <VCol
+          v-for="pokemon in pokemons"
+          :key="pokemon.name"
+          cols="12"
+        >
+          <VCard>
+            <VCardTitle>{{ pokemon.name }}</VCardTitle>
+            <VCardText>{{ pokemon.url }}</VCardText>
+          </VCard>
+        </VCol>
+      </VRow>
+    </div>
+  </VContainer>
 </template>
+
+<style scoped>
+/* 必要に応じてスタイルを調整してください */
+</style>
